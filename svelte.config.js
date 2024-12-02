580fe0922926659218cf6fb1945b7bfe5edab58d
Diego Fornalha <diegofornalha@gmail.com>
2024-12-02 01:51:14 -0300

    mais uma tentativa

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import vercel from "@sveltejs/adapter-vercel";
+import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
-    adapter: vercel(),
+    adapter: adapter(),
   },
 };
 
