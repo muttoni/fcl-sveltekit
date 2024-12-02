4b14b353a93f64a156cf42ecf0e8d1ed448a442b
Diego Fornalha <diegofornalha@gmail.com>
2024-12-02 01:44:06 -0300

    v latest

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from "@sveltejs/adapter-vercel";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-	}
+  preprocess: preprocess(),
+  kit: {
+    adapter: vercel(),
+  },
 };
 
 export default config;
